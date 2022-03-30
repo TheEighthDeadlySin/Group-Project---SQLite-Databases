c1379ec08fb90be7cdb989cb6dd51267c5c4e107
TheEighthDeadlySin <62848638+TheEighthDeadlySin@users.noreply.github.com>
2022-03-30 12:30:05 +0100

    Add sqlite3
    Calls sqlite file and executes its contents (committing changes to the db).

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import sqlite3
 
+connection = sqlite3.connect("./database.db")
+cursor = connection.cursor()
+with open('main.sqlite', 'r') as f:
+    cursor.executescript(f.read())
+connection.commit()
+
+"""
 def egg_function(): # create a function for egg
     print('egg') # give the output to the user
     egg_function() # call the egg function to resend the output the user failed to respond to
 
 egg_function() # call it to start the output
+"""
