541a26cfe51a8903630775ad094e5ca3384ccc5d
Oliver <oliver.ruppert-lingham@outlook.com>
2022-03-30 12:09:53 +0100

    Second fix to the output system
    Another update to the system to ensure that the user cannot miss the output.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sqlite3
 
-def egg_function() # create a function for egg
+def egg_function(): # create a function for egg
     print('egg') # give the output to the user
     egg_function() # call the egg function to resend the output the user failed to respond to
+
+egg_function() # call it to start the output
